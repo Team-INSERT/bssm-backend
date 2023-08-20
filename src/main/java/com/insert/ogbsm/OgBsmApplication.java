734d468fc9c5dde2cff8ec4f7674946cbe746ebb
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-08-21 00:21:50 +0900

    project setting

---
+++ src/main/java/com/insert/ogbsm/OgBsmApplication.java
@@ -0,0 +1,13 @@
+package com.insert.ogbsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgBsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgBsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insert/ogbsm/OgBsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insert.ogbsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgBsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
