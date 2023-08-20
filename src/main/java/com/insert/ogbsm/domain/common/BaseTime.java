7540ef914d1940b881f57709ed7b3fbb58dd1824
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-08-21 08:16:23 +0900

    CHORE: indent

--- src/main/java/com/insert/ogbsm/domain/common/BaseTime.java
+++ src/main/java/com/insert/ogbsm/domain/common/BaseTime.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
 @Getter
--- src/main/java/com/insert/ogbsm/domain/post/Post.java
+++ src/main/java/com/insert/ogbsm/domain/post/Post.java
@@ -3,6 +3,6 @@ package com.insert.ogbsm.domain.post;
 import jakarta.persistence.Entity;
 
 @Entity
-public class Post extends PostBase{
+public class Post extends PostBase {
 
 }
