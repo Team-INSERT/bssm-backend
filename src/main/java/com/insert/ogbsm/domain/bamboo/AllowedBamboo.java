4d4c507b5bcbe86149c692471c273222db5ffcb4
qlido <jason7530@naver.com>
2023-08-29 20:12:34 +0900

    Feat: Define Bamboo Domain

---
+++ src/main/java/com/insert/ogbsm/domain/bamboo/AllowedBamboo.java
@@ -0,0 +1,29 @@
+package com.insert.ogbsm.domain.bamboo;
+
+import com.insert.ogbsm.domain.common.CreatedAt;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AllowedBamboo extends CreatedAt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long allowedAdminId;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private Bamboo bamboo;
+}
---
+++ src/main/java/com/insert/ogbsm/domain/bamboo/repo/BambooRepo.java
@@ -0,0 +1,7 @@
+package com.insert.ogbsm.domain.bamboo.repo;
+
+import com.insert.ogbsm.domain.bamboo.Bamboo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BambooRepo extends JpaRepository<Bamboo, Long> {
+}
