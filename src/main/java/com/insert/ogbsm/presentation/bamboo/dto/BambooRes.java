331ead8fa44188af99584a3c9a6fd0f127278032
qlido <jason7530@naver.com>
2023-08-30 08:26:29 +0900

    Feat: Set Url Authenticated

--- src/main/java/com/insert/ogbsm/presentation/bamboo/dto/BambooRes.java
+++ src/main/java/com/insert/ogbsm/presentation/bamboo/dto/BambooRes.java
@@ -1,9 +1,11 @@
 package com.insert.ogbsm.presentation.bamboo.dto;
 
 import com.insert.ogbsm.domain.bamboo.Bamboo;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class BambooRes {
     private Long id;
     private String content;
--- src/main/java/com/insert/ogbsm/presentation/bamboo/dto/CreateBambooDto.java
+++ src/main/java/com/insert/ogbsm/presentation/bamboo/dto/CreateBambooDto.java
@@ -1,8 +1,10 @@
 package com.insert.ogbsm.presentation.bamboo.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class CreateBambooDto {
+    @NotNull
     private String content;
 }
