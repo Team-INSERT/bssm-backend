2762d57996c84d97c6fc76f97919a75fe6af183b
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-09-05 11:26:40 +0800

    CHORE: 안쓰는 코드 제거

--- src/main/java/com/insert/ogbsm/service/validation/UserValidation.java
+++ src/main/java/com/insert/ogbsm/service/validation/UserValidation.java
@@ -10,7 +10,6 @@ public class UserValidation {
 
     public void checkSameUser(Long id1, Long id2) {
         if (!Objects.equals(id1, id2)) {
-//            throw new AccessDeniedException("Your not a same user");
             throw new EntityNotFoundException("Your not a same user");
         }
     }
