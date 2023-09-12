f8c5f2b28fff540e298d68952ca211270d7fb734
qlido <jason7530@naver.com>
2023-09-12 15:43:53 +0900

    Fix : javax -> jakarta, Gradle Update

--- src/main/java/com/insert/ogbsm/domain/auth/RefreshToken.java
+++ src/main/java/com/insert/ogbsm/domain/auth/RefreshToken.java
@@ -1,11 +1,15 @@
 package com.insert.ogbsm.domain.auth;
 
-import lombok.*;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
-import javax.persistence.Id;
 import java.time.ZonedDateTime;
 
 @Getter
