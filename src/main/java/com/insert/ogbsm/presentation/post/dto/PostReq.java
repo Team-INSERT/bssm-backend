404855ae074d831500fc0dd262d49db438378457
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-09-14 20:11:03 +0900

    UPDATE: 각 post별 필요한 정보 다 있는지 검증

--- src/main/java/com/insert/ogbsm/presentation/post/dto/PostReq.java
+++ src/main/java/com/insert/ogbsm/presentation/post/dto/PostReq.java
@@ -5,11 +5,41 @@ import com.insert.ogbsm.domain.post.category.Category;
 import com.insert.ogbsm.domain.post.values.CodeReview;
 import com.insert.ogbsm.domain.post.values.LostFound;
 import com.insert.ogbsm.domain.post.values.Project;
+import com.insert.ogbsm.infra.error.exception.BsmException;
+import com.insert.ogbsm.infra.error.exception.ErrorCode;
 
 import java.time.LocalDateTime;
 
 public record PostReq(Long id, String title, Category category, String content, String prUrl, Boolean isFinished,
                       String lostThingImage, LocalDateTime startTime, LocalDateTime endTime, String field) {
+
+    public PostReq(Long id, String title, Category category, String content, String prUrl, Boolean isFinished, String lostThingImage, LocalDateTime startTime, LocalDateTime endTime, String field) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.content = content;
+
+        validateAllExist(category, prUrl, isFinished, startTime, endTime, field);
+
+        this.prUrl = prUrl;
+        this.isFinished = isFinished;
+        this.lostThingImage = lostThingImage;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.field = field;
+    }
+
+    private void validateAllExist(Category category, String prUrl, Boolean isFinished, LocalDateTime startTime, LocalDateTime endTime, String field) {
+        if (category == Category.CODE_REVIEW) {
+            if (prUrl == null || isFinished == null) {
+                throw new BsmException(ErrorCode.POST_VALUE_NOT_EXIST);
+            }
+        } else if (category == Category.PROJECT) {
+            if (startTime == null || endTime == null || field == null) {
+                throw new BsmException(ErrorCode.POST_VALUE_NOT_EXIST);
+            }
+        }
+    }
 
     public Post entityToBeCreated(Long userId) {
         Post post = new Post(title, category, content, userId);
