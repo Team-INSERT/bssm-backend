8f9a7af3dd68d385c37f501c656dde03fad04d80
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-09-29 17:49:03 +0900

    FIX: errorCode conflict and record일 때 나는 오류

--- src/main/java/com/insert/ogbsm/infra/error/exception/ErrorCode.java
+++ src/main/java/com/insert/ogbsm/infra/error/exception/ErrorCode.java
@@ -45,7 +45,7 @@ public enum ErrorCode {
     IMAGE_NOT_FOUND(404, "IMAGE-404-1", "Image Not Found"),
 
     //Calender
-    Invalid_Date(400,"CALENDER-400-1","Invalid Date");
+    Invalid_Date(400,"CALENDER-400-1","Invalid Date"),
     NO_AUTH_TO_DEF_CALENDER(403, "CALENDER-403-1", "No Auth To Def Calender"),
     CALENDER_NOT_FOUND(404, "CALENDER-404-1", "Calender Not Found"),
 
