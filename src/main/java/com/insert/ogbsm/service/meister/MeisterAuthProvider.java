9fe8b12df45b4a6b21bc811e92c7c808a74ebb57
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-10-04 15:58:49 +0900

    ADD: meister provider

---
+++ src/main/java/com/insert/ogbsm/service/meister/MeisterAuthProvider.java
@@ -0,0 +1,62 @@
+package com.insert.ogbsm.service.meister;
+
+import com.insert.ogbsm.domain.user.Student;
+import com.insert.ogbsm.infra.error.exception.BsmException;
+import com.insert.ogbsm.infra.error.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class MeisterAuthProvider {
+
+    private final OkHttpClient httpClient;
+    private final String LOGIN_URL = "https://bssm.meistergo.co.kr/inc/common_json.php";
+    private final String LOGOUT_URL = "https://bssm.meistergo.co.kr/logout.php";
+
+    public void login(Student student, String pw) throws IOException {
+        String hakgwa;
+        if (student.getGrade() == 1) {
+            hakgwa = "공통과정";
+        } else if (student.getClassNo() <= 2) {
+            hakgwa = "소프트웨어개발과";
+        } else {
+            hakgwa = "임베디드소프트웨어과";
+        }
+        Request request = new Request.Builder()
+                .url(LOGIN_URL)
+                .post(new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("caseBy", "login")
+                        .addFormDataPart("pw", pw)
+                        .addFormDataPart("lgtype", "S")
+                        .addFormDataPart("hakgwa", hakgwa)
+                        .addFormDataPart("hak", String.valueOf(student.getGrade()))
+                        .addFormDataPart("ban", String.valueOf(student.getClassNo()))
+                        .addFormDataPart("bun", String.valueOf(student.getStudentNo()))
+                        .build())
+                .build();
+
+        Response response = httpClient.newCall(request).execute();
+        if (!Objects.requireNonNull(response.body()).string().equals("true")) {
+            throw new BsmException(ErrorCode.MEISTER_PASSWORD_FORBIDDEN);
+        }
+    }
+
+    public void logout() throws IOException {
+        httpClient.newCall(
+                new Request.Builder()
+                        .url(LOGOUT_URL)
+                        .get()
+                        .build()
+        ).execute();
+    }
+
+}
---
+++ src/main/java/com/insert/ogbsm/service/meister/MeisterDataProvider.java
@@ -0,0 +1,81 @@
+package com.insert.ogbsm.service.meister;
+
+import com.insert.ogbsm.domain.meister.MeisterData;
+import com.insert.ogbsm.domain.meister.MeisterInfo;
+import com.insert.ogbsm.domain.meister.repository.MeisterDataRepository;
+import com.insert.ogbsm.domain.meister.repository.MeisterInfoRepository;
+import com.insert.ogbsm.domain.user.Student;
+import com.insert.ogbsm.infra.error.exception.BsmException;
+import com.insert.ogbsm.infra.error.exception.ErrorCode;
+import com.insert.ogbsm.presentation.meister.dto.response.MeisterDetailResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class MeisterDataProvider {
+
+    private final MeisterInfoFacade meisterInfoFacade;
+    private final MeisterInfoRepository meisterInfoRepository;
+    private final MeisterDataRepository meisterDataRepository;
+    private final MeisterParsingProvider meisterProvider;
+    private final MeisterAuthProvider meisterAuthProvider;
+
+    public MeisterData findOrElseCreateMeisterData(Student student) {
+        return meisterDataRepository.findById(student.getStudentId()).orElseGet(
+                () -> {
+                    MeisterInfo meisterInfo = meisterInfoRepository.save(
+                            MeisterInfo.builder()
+                                    .studentId(student.getStudentId())
+                                    .lastPrivateDate(LocalDateTime.now())
+                                    .email(student.getEmail())
+                                    .build()
+                    );
+                    return MeisterData.builder()
+                            .studentId(student.getStudentId())
+                            .meisterInfo(meisterInfo)
+                            .build();
+                }
+        );
+    }
+
+    public MeisterData getAndUpdateMeisterData(MeisterData meisterData) {
+        MeisterInfo meisterInfo = meisterData.getMeisterInfo();
+
+        MeisterDetailResponse responseDto;
+        try {
+            meisterData.setModifiedAt(LocalDateTime.now());
+            meisterAuthProvider.login(meisterInfoFacade.getStudent(meisterInfo), meisterInfo.getStudentId());
+            responseDto = meisterProvider.getAllInfo(meisterInfoFacade.getStudent(meisterInfo));
+        } catch (BsmException e) {
+            try {
+                meisterInfo.setLoginError(true);
+                responseDto = meisterProvider.getScoreInfo(meisterInfoFacade.getStudent(meisterInfo));
+                meisterData.setScores(responseDto);
+                meisterData.setScoreRawData(responseDto.getScoreHtmlContent());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            meisterInfoRepository.save(meisterInfo);
+            return meisterDataRepository.save(meisterData);
+        } catch (IOException e) {
+            throw new BsmException(ErrorCode.MEISTER_INTERNAL_SERVER);
+        }
+
+        if (meisterInfo.isLoginError()) {
+            meisterInfo.setLoginError(false);
+            meisterInfoRepository.save(meisterInfo);
+        }
+        meisterData.setScores(responseDto);
+        meisterData.setScoreRawData(responseDto.getScoreHtmlContent());
+        meisterData.setPositivePoint(responseDto.getPositivePoint());
+        meisterData.setNegativePoint(responseDto.getNegativePoint());
+        meisterData.setPointRawData(responseDto.getPointHtmlContent());
+
+        return meisterDataRepository.save(meisterData);
+    }
+
+}
