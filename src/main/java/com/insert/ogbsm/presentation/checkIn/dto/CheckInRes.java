2d638757e64774ca340014929c985e20c33d18a1
qlido <jason7530@naver.com>
2023-11-08 20:33:11 +0900

    수정

--- src/main/java/com/insert/ogbsm/presentation/checkIn/dto/CheckInRes.java
+++ src/main/java/com/insert/ogbsm/presentation/checkIn/dto/CheckInRes.java
@@ -3,6 +3,9 @@ package com.insert.ogbsm.presentation.checkIn.dto;
 import com.insert.ogbsm.domain.checkIn.CheckIn;
 import com.insert.ogbsm.domain.room.Room;
 import com.insert.ogbsm.domain.room.type.DormitoryType;
+import com.insert.ogbsm.domain.user.User;
+import com.insert.ogbsm.presentation.user.dto.UserSimpleWithNameRes;
+import java.util.Objects;
 import lombok.Getter;
 
 import java.util.Collections;
@@ -12,18 +15,21 @@ import java.util.List;
 public class CheckInRes {
     String roomNumber;
     DormitoryType dormitoryType;
-    List<Long> roomMates;
+    List<UserSimpleWithNameRes> roomMates;
     List<RoomMateCheckInRes> checkInList;
 
-    public CheckInRes(Room room, List<CheckIn> checkIn) {
+    public CheckInRes(Room room, List<CheckIn> checkIn, List<User> users) {
         this.roomNumber = room.getRoomNumber();
         this.dormitoryType = room.getDormitoryType();
-        this.roomMates = room.getRoomMate().getRoomMateIds().stream().toList();
+        this.roomMates = users.stream().map(UserSimpleWithNameRes::new).toList();
+//        this.roomMates = room.getRoomMate().getRoomMateIds().stream().toList();
 
         try {
-            this.checkInList = checkIn.stream().map(RoomMateCheckInRes::new).toList();
+            this.checkInList = checkIn.stream().map(checkIn1 -> new RoomMateCheckInRes(checkIn1,
+                    users.stream().filter(user -> Objects.equals(user.getId(), checkIn1.getUserId())).toList().get(0))).toList();
         } catch (Exception e) {
             this.checkInList = Collections.emptyList();
         }
     }
+
 }
