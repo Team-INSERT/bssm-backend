36fb74ab00a4466c164ee949ae919601400426b9
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-11-09 23:18:07 +0900

    Refactor(Image): Business의 흐름이 잘 보이도록 Business 구현

--- src/main/java/com/insert/ogbsm/service/image/business/ImageDefBusiness.java
+++ src/main/java/com/insert/ogbsm/service/image/business/ImageDefBusiness.java
@@ -1,16 +1,17 @@
-package com.insert.ogbsm.service.image;
+package com.insert.ogbsm.service.image.business;
 
 import com.insert.ogbsm.infra.s3.service.S3Service;
+import com.insert.ogbsm.service.image.implement.ImageImplement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
-public class ImageDefService {
-    private final S3Service s3Service;
+public class ImageDefBusiness {
+    private final ImageImplement imageImplement;
 
     public String uploadImage(MultipartFile file) {
-        return s3Service.uploadImage(file);
+        return imageImplement.append(file);
     }
 }
